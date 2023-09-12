21a9649bf32de13c4da31eed2f2fc2f854ce437b
Andrew Chung <aandrewchung@gmail.com>
2023-09-12 16:47:08 -0400

    exported fn to b used in other files

--- scripts/gptprompt.js
+++ scripts/gptprompt.js
@@ -1,15 +1,3 @@
-const story = `
-In a sunny, peaceful suburban neighborhood filled with colorful houses and lush gardens, 
-Sarah, a kind-hearted young girl, was playing in her front yard when she suddenly realized 
-her fluffy orange kitten, Whiskers, was missing. Her heart filled with worry, she embarked 
-on a search throughout the neighborhood, calling out Whiskers' name. Her determined quest 
-led her to discover Whiskers hiding under a bush in a neighbor's garden, frightened by a loud noise. 
-With gentle coaxing, Sarah reunited with her beloved pet, and Whiskers purred happily 
-in recognition. Cradling Whiskers in her arms, Sarah comforted him with soft words 
-as they made their way back home. Together, they returned to their front yard, 
-continuing their playtime, their bond stronger than ever.
-`;
-
 const dotenv = require('dotenv');
 dotenv.config(); // Load environment variables from .env
 
@@ -52,13 +40,15 @@ async function generateChatGPTResponse(prompt) {
   }
 }
 
-// Example usage
-const prompt = 'Generate a 50 story about a dragon and a knight';
+module.exports = generateChatGPTResponse; // Export the function
 
-generateChatGPTResponse(prompt)
-  .then(response => {
-    // Handle the ChatGPT response here
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+// Example usage
+// const prompt = 'Generate a 50 story about a dragon and a knight';
+
+// generateChatGPTResponse(prompt)
+//   .then(response => {
+//     // Handle the ChatGPT response here
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
