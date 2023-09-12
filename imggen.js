33d0bd32b1df51898680e63e307ba89e396e73e0
Andrew Chung <aandrewchung@gmail.com>
2023-09-12 08:45:33 -0400

    set up image prompt generator

--- imggen.js
+++ imggen.js
@@ -1,2 +1,30 @@
-//test
+import { config } from 'dotenv';
+import OpenAI from 'openai';
 
+// Load environment variables from .env file
+config();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function generateDALLE2Image(prompt) {
+    try {
+        const response = await openai.images.generate({
+            prompt: "a white siamese cat",
+            n: 1,
+            size: "512x512",
+          });
+
+        const image_url = response.data[0].url;
+          
+        console.log('Generated Image URL:', image_url);
+        return image_url;
+    } catch (error) {
+        console.error('Error generating image:', error);
+    }
+}
+
+const prompt = 'Generate an image of a cute cat in space.';
+
+generateDALLE2Image(prompt);
